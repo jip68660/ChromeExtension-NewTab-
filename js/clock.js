cc58dfdf64daefe0920c631d3b7c8c1d82913248
Seon Gyoo Lee <seongyoolee@gmail.com>
2018-12-28 14:51:18 +0000

    add real time clock function

--- js/clock.js
+++ js/clock.js
@@ -1,16 +1,16 @@
-/**
- * need to make "dateDisplay" and "timeDisplay" text divs in test.html
- * test.html under basic layout construction
- * 
- */
-function displayTime() {
-  var today = new Date("");
+window.onload = setInterval(function() {
+  var today = new Date();
   var currYear = today.getFullYear();
   var currMonth = today.getMonth();
   var currDate = today.getDate();
   var currDay = today.getDay();
   var currHour = today.getHours();
   var currMinute = today.getMinutes();
+  var currSecond = today.getSeconds();
+
+  currHour = checkTime(currHour);
+  currMinute = checkTime(currMinute);
+  currSecond = checkTime(currSecond);
 
   var weekday = new Array(7);
   weekday[0] = "일";
@@ -28,10 +28,17 @@ function displayTime() {
     if (currHour == 0) {
       currHour = 12;
     }
-    var timeOutString = currHour + ":" + currMinute + " AM";
-    document.getElementById("timeDisplay").innerHTML = timeOutString;
+    var timeOutString = currHour + ":" + currMinute + ":" + currSecond + " AM";
   } else {//PM
-    var timeOutString = currHour + ":" + currMinute + "PM";
-    document.getElementById("timeDisplay").innerHTML = timeOutString;
+    var timeOutString = (currHour - 12) + ":" + currMinute + ":" + currSecond + "PM";
   }
+  document.getElementById("timeDisplay").innerHTML = timeOutString;
+}, 500);
+
+function checkTime(i)
+{
+	if (i < 10) {
+	   i="0" + i;
+	}
+	return i;
 }
