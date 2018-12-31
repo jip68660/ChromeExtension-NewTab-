0334368b64f6ef60e1dd05410dcbd88e8467d495
slee3056 <slee3056@gatech.edu>
2018-12-31 15:31:20 +0900

    get weather data from api

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    $('#submitWeather').click(function() {
+        var city = $("#city").val();
+
+        if (city!='') {
+            $.ajax({
+                url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" + "&APPID=3b2a6440c9db33c97812d5b7f73e9206",
+                type: "GET",
+                dataType: "jsonp",
+                success: function(data) {
+                    console.log(data);
+                }
+            });
+        } else {
+            $('#error').html('Field cannot be empty');
+        }
+    });
+
+});
