af24b0e127f457d0e5ea7209c50ea5f1506c29c3
slee3056 <slee3056@gatech.edu>
2019-01-01 10:44:45 +0900

    add weather api, no error

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
 
         if (city!='') {
             $.ajax({
-                url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=metric" + "&APPID=3b2a6440c9db33c97812d5b7f73e9206",
+                url: 'https://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=metric" 
+                + "&APPID=3b2a6440c9db33c97812d5b7f73e9206",
                 type: "GET",
                 dataType: "jsonp",
                 success: function(data) {
