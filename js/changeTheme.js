74b347539d9b434c37449e5effb2141b520f5613
slee3056 <slee3056@gatech.edu>
2019-04-25 09:21:57 +0900

    apply theme colors to 2,3,4,5

--- js/changeTheme.js
+++ js/changeTheme.js
@@ -3,4 +3,16 @@ if (localStorage.background == "img/mainback1.jpg") {
     $("#searchUI").css("box-shadow", "0px 0px 5px #505050");
     $(".fitBGborder").css("background-color", "#84638F");
     $(".fitBGborder").css("border-color", "#84638F");   
-}    
+} else if (localStorage.background == "img/mainback2.jpg") {
+    $(".fitBGborder").css("background-color", "#328ba3");
+    $(".fitBGborder").css("border-color", "#328ba3");   
+} else if (localStorage.background == "img/mainback3.jpg") {
+    $(".fitBGborder").css("background-color", "#001C32");
+    $(".fitBGborder").css("border-color", "#001C32");   
+} else if (localStorage.background == "img/mainback4.jpg") {
+    $(".fitBGborder").css("background-color", "#30517E");
+    $(".fitBGborder").css("border-color", "#30517E");   
+} else if (localStorage.background == "img/mainback5.jpg") {
+    $(".fitBGborder").css("background-color", "#5e5e5e");
+    $(".fitBGborder").css("border-color", "#5e5e5e");   
+} 
