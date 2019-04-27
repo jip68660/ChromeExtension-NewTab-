e875cad6a643eac37c4a30e1e2e6f369ab85bfd5
jihoonshin <jip68660@gmail.com>
2019-04-27 15:10:46 +0900

    add mainItem2 items

--- js/mainItem2.js
+++ js/mainItem2.js
@@ -1,8 +1,11 @@
-var remainContentArr = ["img/knowingBros.png", "img/baekRestaurant.png", "img/food.png", "img/musicBank.png", "img/iLiveAlone.png", "img/rollCall.jpg", "img/running.jpg", "img/religion.png", "img/rollCallNight.jpg"];
+var remainContentArr = ["img/knowingBros.png", "img/baekRestaurant.png", "img/food.png", "img/musicBank.png", "img/iLiveAlone.png", "img/rollCall.jpg", "img/running.jpg", "img/religion.png", "img/rollCallNight.jpg", "img/60seconds.png", "img/history.png"];
 var randomInd = Math.floor(Math.random() * remainContentArr.length);
 var leftCountStr = "번";
 //전역일 오전 9시를 기준으로 잡음
 var endDate = new Date(localStorage.endDateYear, localStorage.endDateMonth - 1, localStorage.endDateDate, 9, 0, 0, 0);
+var totalTime = localStorage.wholeDays;
+var remainTime = localStorage.todoDays;
+var ratio = remainTime / totalTime;
 
 $("#remainContent").click(function() {
     if (!$("#leftCount").hasClass("animated rubberBand")) {
@@ -89,6 +92,8 @@ function currTimeCheck(nextTime, remainCount) {
 
 function leftCountCal(randomIndex) {
     var rightCountStr;
+    $("#rightCount").show();
+    $("#leftCount").removeClass("index10");
     if(localStorage.identity=="girlfriend"){
        rightCountStr = "꽃신!";
     } else {  
@@ -119,15 +124,6 @@ function leftCountCal(randomIndex) {
         $("#title").html("아침점호");
         $("#leftCount").html(remainCount + leftCountStr);
         $("#rightCount").html("더 하면 " + rightCountStr);
-    } else if (randomIndex == 8) { // 저녁점호도 따로 계산
-        leftCountStr = "번";
-        var remainCount = 0;
-        var nextTime = getNextDailyTime(new Date(), 21, 0);
-        remainCount = currTimeCheck(nextTime, remainCount);
-
-        $("#title").html("저녁점호");
-        $("#leftCount").html(remainCount + leftCountStr);
-        $("#rightCount").html("더 하면 " + rightCountStr);
     } else if (randomIndex == 6) { // 뜀걸음
         leftCountStr = "번";
         var remainCount = 0;
@@ -137,6 +133,78 @@ function leftCountCal(randomIndex) {
         $("#title").html("뜀걸음");
         $("#leftCount").html(remainCount + leftCountStr);
         $("#rightCount").html("더 뛰면 " + rightCountStr);
+    } else if (randomIndex == 8) { // 저녁점호도 따로 계산
+        leftCountStr = "번";
+        var remainCount = 0;
+        var nextTime = getNextDailyTime(new Date(), 21, 0);
+        remainCount = currTimeCheck(nextTime, remainCount);
+
+        $("#title").html("저녁점호");
+        $("#leftCount").html(remainCount + leftCountStr);
+        $("#rightCount").html("더 하면 " + rightCountStr);
+    } else if (randomIndex == 9) {  //60초 폭탄
+        leftCountStr = "초";       
+        remainCount = Math.floor(60 * ratio);
+
+        if (remainCount == 0 || remainCount == 1) {
+            $("#title").html("폭탄 폭발")
+            $("#leftCount").html("얼마 안남았습니다!");            
+            $("#rightCount").html("곧 " + rightCountStr);  
+        }
+        else {
+            $("#title").html("폭탄 폭발까지")        
+            $("#leftCount").html(remainCount + leftCountStr);
+            $("#rightCount").html("후면 " + rightCountStr);
+        }    
+    } else if (randomIndex == 10) { //역사?
+        leftCountStr = "시대";
+        ratio = ratio * 100;
+        $("#leftCount").addClass("index10");
+        /* 역사표 (그냥 단순하게 나눔)
+            0-10 : 선사시대
+            11-20 : 고조선시대
+            21-30 : 삼국시대
+            31-40 : 통일신라시대
+            41-50 : 후삼국시대
+            51-60 : 고려시대
+            61-70 : 조선시대
+            71-80 : 대한제국시대
+            81-90 : 대한민국임시정부시대
+            91-100 : 대한민국
+         */
+        if (ratio <= 10) {
+            remainCount = "선사";
+        }else if (ratio <= 20) {
+            remainCount = "고조선"
+        }
+        else if (ratio <= 30) {
+            remainCount = "삼국" 
+        }
+        else if (ratio <= 40) {
+            remainCount = "통일신라"
+        }
+        else if (ratio <= 50) {
+            remainCount = "후삼국"
+        }
+        else if (ratio <= 60) {
+            remainCount = "고려"
+        }
+        else if (ratio <= 70) {
+            remainCount = "조선"
+        }
+        else if (ratio <= 80) {
+            remainCount = "대한제국"
+        }
+        else if (ratio <= 90) {
+            remainCount = "대한민국임시정부"
+        }
+        else {
+            remainCount = "대한민국";
+        }
+        $("#title").html("역사로 치자면");        
+        $("#leftCount").html(remainCount + leftCountStr);
+        $("#rightCount").hide();
+
     } else {//일주일에 한번
         leftCountStr = "번"
         var nextTime = getNextWeeklyTime(new Date());
